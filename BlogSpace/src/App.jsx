ad11da4647dc0c91869c652ce605bf24c356acd4
Abhishek jain <abhishekjain@Abhishek-MacBook-Pro.local>
2025-11-05 13:27:03 +0530

    readding older apps

---
+++ BlogSpace/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Start of React App - BlogSpace</h1>
+      <button><a href='./new_page'>Next page</a></button>
+    </>
+  )
+}
+
+export default App
